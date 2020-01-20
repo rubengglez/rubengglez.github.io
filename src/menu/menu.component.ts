126fff78c39eec7a1b53d6c93a0c358919ff16bb
Rubén García <ruben.garcia@quobis.com>
2020-01-20 19:58:06 +0100

    style: add css rules to better apparience

--- src/menu/menu.component.ts
+++ src/menu/menu.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
 	templateUrl: './menu.component.html',
 	styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {}
+export class MenuComponent {
+	//@TODO: implement active class in menu
+}
