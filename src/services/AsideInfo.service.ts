58b111a7c0f397d73b8b03420433cf94e9e1b30b
Rubén García <ruben.garcia@quobis.com>
2020-02-01 18:02:18 +0100

    feat: make url relatives to base-url

--- src/services/AsideInfo.service.ts
+++ src/services/AsideInfo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const asideInfoUrl = '../assets/asideInfo.json';
+const asideInfoUrl = '/assets/asideInfo.json';
 
 @Injectable({
 	providedIn: 'root',
--- src/services/Experience.service.ts
+++ src/services/Experience.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const experienceUrl = '../assets/experiences.json';
+const experienceUrl = '/assets/experiences.json';
 
 @Injectable({
 	providedIn: 'root',
